ac0e63c31f71f6ace66567182f4278d14e61e032
jose luis <cortezluisjose@gmail.com>
2019-07-02 02:02:01 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  fruits = [3,5,7]
+  square_array(fruits)
+  new_fruits = [7,9,34]
+  square_array(new_fruits)
+  while square_array<= 10 
+  puts square_array
+  square_array += 1 
+  
+  
+  
 end
